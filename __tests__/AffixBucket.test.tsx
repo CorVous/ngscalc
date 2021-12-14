942f71a012038fb0dbcba5b9aaa467da6bda8bae
Cor Vous <vouscor@gmail.com>
2021-12-13 16:48:21 -0800

    adding some tests

--- __tests__/AffixBucket.test.tsx
+++ __tests__/AffixBucket.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import ArmorDisplay from '../src/components/ArmorDisplay'
 import { } from '../src/helpers/HelperTypes'
-import {  } from '../__mocks__/dataMocks'
+import { } from '../__mocks__/dataMocks'
 import { mockTranslation } from '../__mocks__/mockTranslation'
 
 describe.skip('Affix Bucket Component', () => {
@@ -27,7 +27,22 @@ describe.skip('Affix Bucket Component', () => {
   *   what affixes will be in the bucket after confirming selection
   * - If amount of slots reduce below the amount of affixes are in the bucket,
   *   the component should prevent adding new affixes until affixes are removed
+  * 
+  * 
+  * - note: maybe have a seperate AffixZone component that can be inside an affix bucket
   */
-  it('should render the correct number of slots', () => {
+
+  it('should render the correct number of slots with 3 armor', () => {
+    render(<AffixBucket t={mockTranslation} locale="en" 
+      armor={3}
+      weapon={1}
+      armorSlots={6}
+      weaponSlots={2}
+    />)
+    expect(screen.queryByTestId('armor-fixa-slot')).toHaveLength(3)
+    expect(screen.queryByTestId('weapon-fixa-slot')).toHaveLength(1)
+    expect(screen.queryByTestId('armor-affix-slot')).toHaveLength(6)
+    expect(screen.queryByTestId('weapon-affix-slot')).toHaveLength(2)
   })
+
 })
